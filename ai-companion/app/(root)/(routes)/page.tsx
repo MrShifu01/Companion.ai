39bd27cbadec56eaceae9b4ff1b2eca9ac041d0a
MrShifu01 <stander.christian.com>
2023-08-21 07:35:00 +0200

    Clerk Auth Working great

---
+++ ai-companion/app/(root)/(routes)/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { UserButton } from "@clerk/nextjs";
+
+export default function RootPage() {
+  return (
+    <div>
+      <UserButton afterSignOutUrl="/"/>
+    </div>
+  )
+}
